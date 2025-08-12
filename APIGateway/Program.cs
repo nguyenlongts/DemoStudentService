01ed6bcb96e55b6ffb83320406ae7af9e5e46086
Long <Long@DESKTOP-CMCEQ5O>
2025-08-12 14:37:59 +0700

    add api gateway

---
+++ APIGateway/Program.cs
@@ -0,0 +1,10 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+IConfiguration config = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+builder.Services.AddOcelot(config);
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
+app.Run();
